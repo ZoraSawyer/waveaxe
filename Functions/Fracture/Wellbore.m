b3196259c23305f44225783432fbdb8ed064f3f8
endrinar <endrinita@gmail.com>
2021-01-13 06:11:58 -0500

    Clean up Wellbore

--- Functions/Fracture/Wellbore.m
+++ Functions/Fracture/Wellbore.m
@@ -11,17 +11,6 @@ if strcmp(mode,'length')
     nu = Material.solid.constitutive.constant(2);   % Poisson's ratio of the solid [-]
     mu = Material.fluid.mu;                         % fluid viscosity [Pa.s]
 
-    % In situ stress field 
-    % S0  = [Domain.InsituStress.Sx,  Domain.InsituStress.Sxy;
-    %        Domain.InsituStress.Sxy, Domain.InsituStress.Sy];
-
-    % ncr = CMesh.surface_normal(1,:);   
-    % S1  = S0(1,1)/2+S0(2,2)/2 + abs(S0(1,1)/2-S0(2,2)/2)*(ncr(2)^2 - ncr(1)^2);
-    % S2  = S0(1,1)/2+S0(2,2)/2 - abs(S0(1,1)/2-S0(2,2)/2)*(ncr(2)^2 - ncr(1)^2);
-
-    % Smin = min(abs(S1),abs(S2));
-    % Smin = S0(1,1)/2+S0(2,2)/2 + abs(S0(1,1)/2-S0(2,2)/2)*(ncr(2)^2 - ncr(1)^2);
-
     q = 1e-4;       % injection flux [m^2/s]
     h = 1;          % fracture height [m]
     Q = q*h;        % injection flow rate [m^3/s]
